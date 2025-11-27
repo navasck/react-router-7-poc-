47e4b4ce1bc26c2960148469d5d16c82bc1d93bc
ABDUL NAVAS CK <cknavas123@gmail.com>
2025-11-27 19:13:00 +0530

    sw version changed

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/sw.js")
+      .register("/sw.js?v=2")
       .then(() => console.log("Service Worker registered"))
       .catch(err => console.log("SW registration failed", err));
   });
